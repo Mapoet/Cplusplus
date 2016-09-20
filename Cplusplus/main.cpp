4bda490c340d6d64051b6235e06faf198d53e12c
Mapoet Niphy <nffu@shao.ac.cn>
2016-09-20 11:29:20 +0800

    Initial Commit

---
+++ Cplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cplusplus
+//
+//  Created by Mapoet Niphy on 16/9/20.
+//  Copyright © 2016年 Mapoet Niphy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
